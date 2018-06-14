eb391eae8aa6716439aa8343d0e5fbc9ce384fb5
Maciej <maciej@myx.systems>
2018-06-14 21:57:59 +1000

    Initial Commit

---
+++ MXSErrorControl/MXSErrorControl.h
@@ -0,0 +1,19 @@
+//
+//  MXSErrorControl.h
+//  MXSErrorControl
+//
+//  Created by Maciej Necki on 14/6/18.
+//  Copyright © 2018 MYX Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MXSErrorControl.
+FOUNDATION_EXPORT double MXSErrorControlVersionNumber;
+
+//! Project version string for MXSErrorControl.
+FOUNDATION_EXPORT const unsigned char MXSErrorControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MXSErrorControl/PublicHeader.h>
+
+
